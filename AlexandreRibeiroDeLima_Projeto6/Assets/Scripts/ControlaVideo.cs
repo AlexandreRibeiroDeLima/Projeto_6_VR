5bb74a2b323cad27371e8b206e10513eec7c659b
Alexandre <alexandre.lima@iag.usp.br>
2019-04-12 17:35:18 -0300

    Projeto_6_VR

---
+++ AlexandreRibeiroDeLima_Projeto6/Assets/Scripts/ControlaVideo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaVideo : MonoBehaviour {
+
+	public GameObject videoPlayer;
+
+	// Use this for initialization
+	void Start () {
+
+	
+		
+	}
+	
+
+	public void rodarVideo () {
+		
+		Debug.Log ("teste");
+	
+
+	}
+
+}
---
+++ AlexandreRibeiroDeLima_Projeto6/Assets/Scripts/displayFPS.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class displayFPS : MonoBehaviour {
+
+	public Text text;
+	private float fps;
+	private float deltaTime;
+
+	void Start() {
+		deltaTime = 0.0f;
+	}
+
+	void Update() {
+		// Calculates the number of frames each second at the given frame rate. We take or 1 second divided by our delta time (the time since the frame was rendered) to see how many frames we could render each second if each frame took the same time. 
+		fps = Mathf.FloorToInt(1.0f / deltaTime);
+		text.text = fps.ToString();
+
+		deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+	}
+
+}
